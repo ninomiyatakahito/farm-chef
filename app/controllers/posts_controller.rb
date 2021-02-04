82102c6b0fe39e6c0fc5050c750258243dd998e8
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-04 20:34:54 +0900

    Active Strageの導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
   def index
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:post_text, :image).merge(user_id: current_user.id)
+  end
 end
