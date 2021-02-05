f5ffe2bb03abda8706b7db349b3066bc75b9320b
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-06 03:21:47 +0900

    アクション修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
     @name = user.last_name
-    @profile = user.profile
     @prefecture = user.prefecture
     @business_name = user.business_name
     @profile = user.profile
