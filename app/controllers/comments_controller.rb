57aceb12a5443be41fc4788c9887bbda4362af38
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-05 16:26:40 +0900

    アクション定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post)
+      else
+      render "prototypes/show"
+    end
+  end
+
+  
 end
