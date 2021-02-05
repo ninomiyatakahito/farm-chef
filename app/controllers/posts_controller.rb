5f4a8a74b47d01355c158c3f88685a1f6ad7d1d8
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-05 15:59:58 +0900

    編集、更新、削除のアクション定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,27 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    unless current_user.id == @post.user.id
+      redirect_to action: :index 
+    end 
+  end
+
+  def update
+    if post = Post.find(params[:id])
+      post.update(post_params)
+      redirect_to post_path (post.id), method: :get
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
+  end
 
   private
 
