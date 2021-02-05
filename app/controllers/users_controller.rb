8edcf54ca42d08c252a4f88985803e96cd31854e
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-06 03:17:00 +0900

    詳細に必要なアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @name = user.last_name
+    @profile = user.profile
+    @prefecture = user.prefecture
+    @business_name = user.business_name
+    @profile = user.profile
   end
 
 end
