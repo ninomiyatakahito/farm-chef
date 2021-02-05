04a2259ab81046c66fd2884ac24c3b0164185e18
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-05 16:29:38 +0900

    ストロングパラメーターの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,9 @@ class CommentsController < ApplicationController
       render "prototypes/show"
     end
   end
-
   
+  private
+  def comment_params
+    params.require(:comment).permit(:comment_text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
 end
