ff5b1de51458123fb258b95302ebdd9fee32d19c
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-06 23:20:28 +0900

    ユーザーにプロフィール画像を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
          has_many :posts, dependent: :destroy
          has_many :comments , dependent: :destroy
          
-         #has_one_attached :avatar  #ユーザーのアバター
+         has_one_attached :avatar  #ユーザーのアバター
 end
