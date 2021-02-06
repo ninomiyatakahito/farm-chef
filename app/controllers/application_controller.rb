4087c178f695ed1d2ed11170ee1a4abfe1dbcebd
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-06 22:19:41 +0900

    編集後のストロングパラメーター設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :post_code, :prefecture, :city, :street, :business_name, :profile])
-    #devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :post_code, :prefecture, :city, :street, :business_name, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :post_code, :prefecture, :city, :street, :business_name, :profile])
   end
 end
