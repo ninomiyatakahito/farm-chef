0372806ed3ffe03560fb0b40d20604a5c41a0ee0
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-07 11:26:49 +0900

    postのアソシエーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one_attached :image
 
+  has_many :likes　　#いいね機能
+  has_many :liked_users, through: :likes, source: :user　　#同上
+
   validates :post_text, presence: true
   validates :image, presence: true
 end
