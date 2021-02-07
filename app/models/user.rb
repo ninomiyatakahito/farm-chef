66b2504943ddff28ce93f6ea72bfc9cf3086b880
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-07 23:36:46 +0900

    アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,14 @@ class User < ApplicationRecord
          has_one_attached :avatar 
 
          has_many :likes, dependent:  :delete_all
-         has_many :liked_posts, through: :likes, source: :post　
+         has_many :liked_posts, through: :likes, source: :post
+
+         has_many :relationships
+         has_many :followings, through: :relationships, source: :follow
+         has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+         has_many :followers, through: :reverse_of_relationships, source: :user
+
+         
 
   def already_liked?(post)                #ユーザーが投稿に対して、すでにいいねをしているのかどうかを判定
     self.likes.exists?(post_id: post.id)
