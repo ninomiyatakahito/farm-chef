5c186774f17a7787d0a15f79c73707430e00029a
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-07 11:25:31 +0900

    アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
          has_many :comments , dependent: :destroy
          
          has_one_attached :avatar  #ユーザーのアバター
+
+         has_many :likes, dependent: :destroy　#いいね機能 記述はあとで消す
+         has_many :liked_posts, through: :likes, source: :post　#いいね機能
 end
