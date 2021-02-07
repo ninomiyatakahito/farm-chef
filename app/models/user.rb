6e7463aa6a83f649e75c2058039e182d8b67e2c8
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-07 23:42:06 +0900

    メソッド記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,20 @@ class User < ApplicationRecord
          has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
          has_many :followers, through: :reverse_of_relationships, source: :user
 
-         
+        def follow(other_user)
+          unless self == other_user
+            self.relationships.find_or_create_by(follow_id: other_user.id)
+          end
+        end
+    
+        def unfollow(other_user)
+          relationship = self.relationships.find_by(follow_id: other_user.id)
+          relationship.destroy if relationship
+        end
+      
+        def following?(other_user)
+          self.followings.include?(other_user)
+        end
 
   def already_liked?(post)                #ユーザーが投稿に対して、すでにいいねをしているのかどうかを判定
     self.likes.exists?(post_id: post.id)
