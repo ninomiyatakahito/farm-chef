c557bc334cbb01e93eda1104bc2e73a34d1b8bed
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-09 23:23:32 +0900

    フォローカウント

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,5 @@
 class RelationshipsController < ApplicationController
-  before_action :set_user
+  before_action :set_user, only: [:create, :destroy]
 
   def create
     following = current_user.follow(@user)
@@ -25,7 +25,7 @@ class RelationshipsController < ApplicationController
 
   private
 
-  def
-    @user = User.find(params[:relationship][:follow_id])
+  def set_user
+    @user = User.find(params[:follow_id])
   end
 end
