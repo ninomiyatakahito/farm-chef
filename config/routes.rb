3a9e0477dbf204a6cdd2257ac57a290f143f7b98
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-10 00:13:31 +0900

    トップページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
-  root to:'posts#index'
+  root to:'posts#home'
   resources :posts do
     resources :comments
     resources :likes
+    member do
+      get 'home'
+    end
   end
   resources :users
   resources :relationships, only: [:create, :destroy]
