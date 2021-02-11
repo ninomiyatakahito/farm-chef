aefb81bd4027f86bac0437f2667d9e09ce9e2e22
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-12 01:22:00 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
   get 'followings/index'
   devise_for :users
   get 'posts/index'
