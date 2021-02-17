75349a8fd49051e9e46cb806892fa549159c7a82
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-17 23:04:36 +0900

    プルダウンメニューの非同期処理

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function(){
 
   const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")/**プルダウンメニューの定義 */
 
   pullDownButton.addEventListener('mouseover', function(){
     pullDownButton.setAttribute("style", "background-color:#FFBEDA;")/*マウスオーバーの色 */
@@ -11,6 +12,12 @@ window.addEventListener('load', function(){
   })
 
   pullDownButton.addEventListener('click', function() {
-    console.log("click OK")/*クリックした時の処理 */
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      // pullDownParentsにdisplay:block;が付与されている場合,実行される
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      // pullDownParentsにdisplay:block;が付与されていない場合実行される
+      pullDownParents.setAttribute("style", "display:block;")
+    }/*プルダウンメニューが表示される。クリックした時の処理 */
   })
 })
